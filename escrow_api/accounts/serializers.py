11b37fe3801047fa97e6181777ad7adc3ab84274
ke-kaa <amsalukaku122@gmail.com>
2025-06-22 01:14:17 +0300

    Update CustomTokenObtainSerializer. Create RegistrationSerializer, UserProfileSerializer, CustomToeknObtainPairView, RegistrationAPIView, UserProfileRetrieveUpdateView

--- escrow_api/accounts/serializers.py
+++ escrow_api/accounts/serializers.py
@@ -14,7 +14,7 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
         credentials = {
             'email': attrs.get('email'),
-            'passowrd': attrs.get('password')
+            'password': attrs.get('password')
         }
 
         user = authenticate(**credentials)
@@ -28,21 +28,34 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     
 
 class RegistrationSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
-    confirm_password = serializers.CharField(write_only=True)
+    password = serializers.CharField(required=True, write_only=True)
+    confirm_password = serializers.CharField(required=True, write_only=True)
 
     class Meta:
         model = my_models.CustomUser
-        fields = ['user_type', 'phone_number', 'country', 'email', 'passowrd', 'confirm_password']
+        fields = ['first_name', 'last_name', 'user_type', 'phone_number', 'country', 'email', 'password', 'confirm_password']
+        extra_kwargs = {
+            'first_name': {'required': True},
+            'last_name': {'required': True},
+            'email': {'required': True},
+            'country': {'required': True},
+        }
     
     def validate(self, attrs):
-        if attrs['password'] != attrs['confirm_passowrd']:
+        if attrs['password'] != attrs['confirm_password']:
             raise serializers.ValidationError("Password do not match.")
         
         return attrs
     
     def create(self, validated_data):
         validated_data.pop('confirm_password')
-        user = my_models.CustomUser.objects.create(**validated_data)
+        user = my_models.CustomUser.objects.create_user(**validated_data)
 
         return user
+    
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = my_models.CustomUser
+        fields = ('id', 'first_name', 'last_name', 'email', 'phone_number', 'user_type', 'country')
+        read_only_fields = ('email',)
--- escrow_api/accounts/views.py
+++ escrow_api/accounts/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
+from rest_framework_simplejwt import views as jwt_views, tokens, authentication
+from rest_framework import views as drf_Views, generics, permissions, response, status
+from django.db import transaction
 
-# Create your views here.
+
+from . import serializers as my_serializers
+
+
+class CustomTokenObtainPairView(jwt_views.TokenObtainPairView):
+    serializer_class = my_serializers.CustomTokenObtainPairSerializer
+
+
+class RegistrationAPIView(generics.CreateAPIView):
+    serializer_class = my_serializers.RegistrationSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        with transaction.atomic():
+            user = serializer.save()
+            refresh = tokens.RefreshToken.for_user(user)
+
+        return response.Response(
+            {
+                'user': serializer.data,
+                'refresh': str(refresh),
+                'access': str(refresh.access_token)
+            },
+            status=status.HTTP_201_CREATED
+        )
+    
+
+class UserProfileRetrieveUpdateAPIView(generics.RetrieveUpdateAPIView):
+    serializer_class = my_serializers.UserProfileSerializer
+    authentication_classes = [authentication.JWTAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
