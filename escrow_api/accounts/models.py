65e51bd83d5de9a99f5f40264c5c7513815e3e17
ke-kaa <amsalukaku122@gmail.com>
2025-06-22 01:11:06 +0300

    Create CustomUserManager. Update CustomUser model

--- escrow_api/accounts/models.py
+++ escrow_api/accounts/models.py
@@ -3,22 +3,43 @@ from django.contrib.auth.models import AbstractUser, BaseUserManager, Permission
 from country_list import countries_for_language
 
 
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Email is required.")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        return self.create_user(email, password, **extra_fields)
+
+
+
 class CustomUser(AbstractUser):
     USER_TYPE_CHOICES = (
         ('freelancer', 'Freelancer'),
         ('client', 'Client'),
-        ('admin', 'Admin'),
     )
 
     user_type = models.CharField(max_length=20, choices=USER_TYPE_CHOICES)
     phone_number = models.CharField(max_length=20, blank=True)
-    country = models.CharField(max_length=50, choices=countries_for_language)
+    country = models.CharField(max_length=50, choices=countries_for_language('en'))
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    email = models.EmailField(unique=True)
+    email = models.EmailField(unique=True, blank=False)
+    username = None
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
+    REQUIRED_FIELDS = ['first_name', 'last_name',]
+
+    objects = CustomUserManager()
 
     def __str__(self):
         return self.email
