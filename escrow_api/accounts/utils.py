305f64e8d627ac9dec6b712e138dc3d0135e91dd
ke-kaa <amsalukaku122@gmail.com>
2025-07-03 19:20:23 +0300

    Implement generate reactivation link and send reactivation email functions

--- escrow_api/accounts/utils.py
+++ escrow_api/accounts/utils.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
 from django.core.mail import send_mail
-from django.utils.http import urlsafe_base64_encode
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.utils.encoding import force_bytes
 from django.conf import settings
 from django.urls import reverse
@@ -40,3 +40,33 @@ def send_reset_email(user, reset_url):
         fail_silently=False,
     )
 
+
+def generate_reactivation_link(user):
+    token = PasswordResetTokenGenerator().make_token(user)
+    uid = urlsafe_base64_encode(force_bytes(user.pk))
+    url = f'{settings.FRONTEND_DOMAIN}/reactivate-account?uid={uid}&token={token}'
+
+    return url, token, uid
+
+
+def send_reactivation_email(user, reactivation_link):
+    subject = "Reactivate Your Account"
+    message = f"""
+    Hi {user.get_full_name() or user.email},
+
+    You requested to reactivate your account.
+
+    Click the link below to proceed:
+    {reactivation_link}
+
+    If you didn’t request this, just ignore this email.
+
+    — {settings.SITE_NAME} Team
+    """
+    send_mail(
+        subject=subject,
+        message=message.strip(),
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        recipient_list=[user.email],
+        fail_silently=False
+    )
