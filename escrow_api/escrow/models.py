e94c265609aefefa8a79bf876be7411875a8572e
ke-kaa <amsalukaku122@gmail.com>
2025-07-03 19:21:59 +0300

    Create escrow app

---
+++ escrow_api/escrow/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+
+from UserProjects.models import UserProject
+
+# Create your models here.
+class EscrowTransaction(models.Model):
+    STATUS_CHOICES = (
+        ('funded', 'Funded'),
+        ('released', 'Released'),
+        ('refunded', 'Refunded'),
+        ('disputed', 'Disputed'),
+    )
+
+    project = models.OneToOneField(UserProject, on_delete=models.CASCADE)
+    balance = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    is_locked = models.BooleanField(default=False)  # Lock during disputes
+    commission_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    # stripe_payment_intent_id = models.CharField(max_length=255, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Escrow for {self.project.title} ({self.amount})"
