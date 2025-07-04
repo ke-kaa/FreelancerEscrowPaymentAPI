0482ada6792e3690f70314090ec5ca93e5f3b3d5
ke-kaa <amsalukaku122@gmail.com>
2025-07-04 20:49:27 +0300

    Create CreateProposalSerializer and CreateProposalAPIView

--- escrow_api/user_projects/serializers.py
+++ escrow_api/user_projects/serializers.py
@@ -42,7 +42,6 @@ class ListProjectFreelancerSerializer(serializers.ModelSerializer):
         fields = ['id', 'client', 'title', 'freelancer', 'description', 'amount', 'commission_rate', 'status', 'created_at', 'updated_at']
 
 
-
 class RetrieveUpdateDeleteProjectClientSeriailzer(serializers.ModelSerializer):
     client = serializers.SerializerMethodField()
     freelancer = serializers.SerializerMethodField()
@@ -65,3 +64,25 @@ class RetrieveUpdateDeleteProjectClientSeriailzer(serializers.ModelSerializer):
         model = my_models.UserProject
         fields = ['id', 'client', 'freelancer', 'title', 'description', 'amount', 'commission_rate', 'status', 'created_at', 'updated_at', 'is_public']
         read_only_fields = ('id', 'freelancer', 'commission_rate', 'status', 'updated_at', )
+
+
+class CreateProposalSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = my_models.Proposal
+        fields = ['cover_letter', 'bid_amount', 'estimated_delivery_days', 'is_withdrawn']
+        extra_kwargs = {
+            'bid_amount': {'required': True},
+            'cover_letter': {'required': True}
+        }
+
+    def validate_bid_amount(self, value):
+        if value < 0:
+            raise serializers.ValidationError("Please enter a valid bid amount.")
+        return value
+    
+    def validate(self, attrs):
+        request = self.context['request']
+        project = self.context['project']
+
+        if my_models.Proposal.objects.filter(project=project, freelancer=request.user).exists():
+            raise serializers.ValidationError("You have already submitted a proposal for this project.")
--- escrow_api/user_projects/views.py
+++ escrow_api/user_projects/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework import views as drf_views, generics, permissions, status
 from rest_framework.response import Response
+from rest_framework_simplejwt import authentication
 from django.shortcuts import get_object_or_404
 
 from . import serializers as my_serializers, permissions as my_permissions, models as my_models
@@ -9,6 +10,7 @@ from . import serializers as my_serializers, permissions as my_permissions, mode
 class CreateProjectAPIView(generics.CreateAPIView):
     serializer_class = my_serializers.CreateProjectSerializer
     permission_classes = [permissions.IsAuthenticated, my_permissions.IsClient]
+    authentication_classes = [authentication.JWTAuthentication]
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data, context={'request': request})
@@ -24,13 +26,15 @@ class CreateProjectAPIView(generics.CreateAPIView):
 class ListProjectAdminAPIView(generics.ListAPIView):
     serializer_class = my_serializers.ListProjectAdminSerializer
     permission_classes = [permissions.IsAuthenticated, permissions.IsAdminUser]    
+    authentication_classes = [authentication.JWTAuthentication]
     queryset = my_models.UserProject.objects.all()
 
 
 class ListProjectClientAPIView(generics.ListAPIView):
     serializer_class = my_serializers.ListProjectClientSerializer
     permission_classes = [permissions.IsAuthenticated, my_permissions.IsClient]
-    
+    authentication_classes = [authentication.JWTAuthentication]
+
     def get_queryset(self):
         return my_models.UserProject.objects.filter(client=self.request.user)
 
@@ -38,15 +42,37 @@ class ListProjectClientAPIView(generics.ListAPIView):
 class ListProjectFreelancerAPIView(generics.ListAPIView):
     serializer_class = my_serializers.ListProjectFreelancerSerializer
     permission_classes = [permissions.IsAuthenticated, my_permissions.IsFreelancer]
+    authentication_classes = [authentication.JWTAuthentication]
     
     def get_queryset(self):
-        return my_models.UserProject.objects.filter(is_public=True)
+        return my_models.UserProject.objects.filter(is_public=True).filter(freelancer__isnull=True)
     
 
 class RetrieveUpdateDeleteProjectClientAPIView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = my_serializers.RetrieveUpdateDeleteProjectClientSeriailzer
     permission_classes = [permissions.IsAuthenticated, my_permissions.IsClient, my_permissions.IsOwner]
+    authentication_classes = [authentication.JWTAuthentication]
     lookup_field = 'id'
 
     def get_object(self):
         return get_object_or_404(my_models.UserProject, id=self.kwargs['id'], client=self.request.user)
+
+
+class CreateProposalAPIView(generics.CreateAPIView):
+    serializer_class = my_serializers.CreateProposalSerializer
+    permission_classes = [my_permissions.IsFreelancer, permissions.IsAuthenticated]
+    authentication_classes = [authentication.JWTAuthentication]
+
+    def get_poject(self):
+        return get_object_or_404(my_models.Proposal, id=self.kwargs['project_id'])
+    
+    def create(self, request, *args, **kwargs):
+        project = self.get_project()
+
+        serializer = self.get_serializer(data=request.data, context={'project': project, 'request': request})
+        serializer.is_valid(raise_exception=True)
+        serializer.save(project=project, freelancer=request.user)
+
+        return Response({
+            'detail': "Proposal submitted successfully."
+        }, status=status.HTTP_201_CREATED)
