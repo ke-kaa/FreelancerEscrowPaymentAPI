efa7ac74c4da2f3febeda7ecc459bbf3a3546e2e
ke-kaa <amsalukaku122@gmail.com>
2025-07-04 12:35:15 +0300

    Create custom permissions: IsClient, IsOwner

---
+++ escrow_api/user_projects/permissions.py
@@ -0,0 +1,11 @@
+from rest_framework.permissions import BasePermission
+
+
+class IsClient(BasePermission):
+    def has_permission(self, request, view):
+        return request.user and request.user.user_type == 'client'
+
+
+class IsOwner(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.client == request.user
