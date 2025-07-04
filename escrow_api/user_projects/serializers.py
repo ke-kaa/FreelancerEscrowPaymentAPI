ef10b0d8b76bd3fa8b75ebdc54d2084ba842230c
ke-kaa <amsalukaku122@gmail.com>
2025-07-04 12:36:13 +0300

    Create CreateProjectSerializer and CreateProjectAPIView

---
+++ escrow_api/user_projects/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+
+
+from . import models as my_models
+
+
+class CreateProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = my_models.UserProject
+        fields = ['title', 'description', 'amount',]
+    
+    def validate_amount(self, value):
+        if value < 0:
+            raise serializers.ValidationError("Please enter a valid amount.")
+        return value
+
+    def create(self, validated_data):
+        request = self.context.get('request')
+        user = request.user
+
+        return my_models.UserProject.objects.create(
+            client=user, 
+            **validated_data
+        )
--- escrow_api/user_projects/views.py
+++ escrow_api/user_projects/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework import views as drf_views, generics, permissions, status
+from rest_framework.response import Response
 
-# Create your views here.
+
+from . import serializers as my_serializers
+from . import permissions as my_permissions
+
+
+class CreateProjectAPIView(generics.CreateAPIView):
+    serializer_class = my_serializers.CreateProjectSerializer
+    permission_classes = [permissions.IsAuthenticated, my_permissions.IsClient]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+
+        return Response({
+            'detail': "Project created successfully.",
+            'project': serializer.data
+        }, status=status.HTTP_201_CREATED)
+    
+
+
+    
