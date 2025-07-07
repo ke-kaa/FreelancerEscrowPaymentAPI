bbb9dbd51a967b0be1c57e0de12f5cfcdac5470d
ke-kaa <amsalukaku122@gmail.com>
2025-07-08 01:54:00 +0300

    Add IsClientOrAssignedFreelancer permission class

--- escrow_api/user_projects/permissions.py
+++ escrow_api/user_projects/permissions.py
@@ -1,4 +1,5 @@
 from rest_framework.permissions import BasePermission
+from .models import UserProject
 
 
 class IsClient(BasePermission):
@@ -14,3 +15,24 @@ class IsOwner(BasePermission):
 class IsFreelancer(BasePermission):
     def has_permission(self, request, view):
         return request.user and request.user.user_type == 'freelancer'
+
+
+class IsClientOrAssignedFreelancer(BasePermission):
+    """"
+    Allows access only to the client or the assigned freelancer of the project.
+    Expects the view to have 'project_id' in kwargs.
+    """
+
+    def has_permission(self, request, view):
+        project_id = view.kwargs.get('project_id')
+        if not project_id:
+            return False
+        try:
+            project = UserProject.objects.get(id=project_id)
+        except UserProject.DoesNotExist:
+            return False
+        
+        user = request.user
+        return project.client == user or project.freelancer == user
+
+        
