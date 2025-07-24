9bf4fbdbcff5561180e48e82138c8f16833b8291
ke-kaa <amsalukaku122@gmail.com>
2025-07-24 19:33:23 +0300

    Add IsModerator and IsDisputeParticipantOrModerator permission classes

---
+++ escrow_api/disputes/permissions.py
@@ -0,0 +1,27 @@
+from rest_framework.permissions import BasePermission
+from .models import Dispute
+
+class IsModerator(BasePermission):
+    """
+    Allows access only to users in the 'Moderators' group.
+    """
+    def has_permission(self, request, view):
+        if not request.user.is_authenticated:
+            return False
+        return request.user.groups.filter(name='Moderators').exists()
+
+
+class IsDisputeParticipantOrModerator(BasePermission):
+    """
+    Allows access only to the project's client, freelancer, or a moderator.
+    This permission is checked against a single Dispute object.
+    """
+    def has_object_permission(self, request, view, obj: Dispute):
+        user = request.user
+        project = obj.project
+        
+        is_participant = (user == project.client or user == project.freelancer)
+        is_moderator = user.groups.filter(name='Moderators').exists()
+        
+        return is_participant or is_moderator
+
