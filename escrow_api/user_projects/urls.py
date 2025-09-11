3041d6f611f3a85811a22765a9b987d39c8e0d48
ke-kaa <amsalukaku122@gmail.com>
2025-09-11 09:31:22 +0300

    create urls for milestone views

--- escrow_api/user_projects/urls.py
+++ escrow_api/user_projects/urls.py
@@ -4,14 +4,19 @@ from . import views as my_views
 
 urlpatterns = [
     # Project endpoints
-    path('client/projects/create/', my_views.CreateProjectClientAPIView.as_view(), name='create-project-client'),
-    path('client/projects/list/', my_views.ListProjectClientAPIView.as_view(), name='list-projects-client'),
-    path('freelancer/projects/list/', my_views.ListProjectFreelancerAPIView.as_view(), name='list-projects-freelancer'),
-    path('admin/projects/list/', my_views.ListProjectAdminAPIView.as_view(), name='list-projects-admin'),
+        # client --tested
+    path('client/create/', my_views.CreateProjectClientAPIView.as_view(), name='create-project-client'),
+    path('client/list/', my_views.ListProjectClientAPIView.as_view(), name='list-projects-client'),
+        # freelancer ---tested
+    path('freelancer/list/', my_views.ListProjectFreelancerAPIView.as_view(), name='list-projects-freelancer'),
+    path('admin/list/', my_views.ListProjectAdminAPIView.as_view(), name='list-projects-admin'),
 
     # Project detail endpoints
+        # client --tested
     path('client/projects/<int:id>/', my_views.RetrieveUpdateDeleteProjectClientAPIView.as_view(), name='retrieve-update-delete-project-client'),
+        # freelancer ---tested
     path('freelancer/projects/<int:id>/', my_views.RetrieveProjectFreelancerAPIView.as_view(), name='retrieve-project-freelancer'),
+        # admin
     path('admin/projects/<int:id>/', my_views.RetrieveProjectAdminAPIView.as_view(), name='retrieve-project-admin'),
 
     # Proposal endpoints
@@ -26,4 +31,20 @@ urlpatterns = [
     path('client/proposals/<int:id>/reject/', my_views.RejectProposalClientAPIView.as_view(), name='reject-proposal-client'),
     path('freelancer/proposals/<int:id>/', my_views.RetrieveUpdateProposalFreelancerAPIView.as_view(), name='retrieve-update-proposal-freelancer'),
     path('freelancer/proposals/<int:id>/withdraw/', my_views.WithdrawProposalFreelancerAPIView.as_view(), name='withdraw-proposal-freelancer'),
+
+    # Milestone endpoints
+    path('client/projects/<int:project_id>/milestone/create/', my_views.CreateMilestoneClientAPIView.as_view(), name='create-milestone-client'),
+    path('client/projects/<int:project_id>/milestones/', my_views.ListProjectMilestonesClientFreelancerAPIView.as_view(), name='list-milestones-client-freelancer'),
+    path('client/projects/<int:project_id>/milestones/<int:id>/', my_views.RetrieveUpdateDeleteMilestoneClientAPIView.as_view(), name='retrieve-update-delete-milestone-client'),
+    path('client/projects/<int:project_id>/milestones/<int:id>/reject/', my_views.RejectMilestoneClientAPIView.as_view(), name='reject-milestone-client'),
+    path('client/projects/<int:project_id>/milestones/<int:id>/approve/', my_views.ApproveMilestoneClientAPIView.as_view(), name='approve-milestone-client'),
+    
+    path('freelancer/projects/<int:project_id>/milestones/', my_views.ListProjectMilestonesClientFreelancerAPIView.as_view(), name='list-milestones-client-freelancer'),
+    path('freelancer/projects/<int:project_id>/milestones/<int:id>/', my_views.RetrieveMilestoneFreelancerAPIView.as_view(), name='retrieve-milestone-freelancer'),
+    path('freelancer/projects/<int:project_id>/milestones/<int:id>/submit/', my_views.SubmitMilestoneFreelancerAPIView.as_view(), name='submit-milestone-freelancer'),
+
+    # Review endpoints
+    path('projects/<int:project_id>/review/submit/', my_views.SubmitReviewAPIView.as_view(), name='submit-review'),
+    path('reviews/<int:id>/', my_views.RetrieveProjectReviewAPIView.as_view(), name='retrieve-project-review'),
+    path('reviews/<int:id>/update/', my_views.UpdateReviewAPIView.as_view(), name='update-project-review'),
 ]
